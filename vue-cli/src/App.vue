f6f929e7ecdab0852842ba9e9d2c6ac90ef6418e
Positive-Conative <kyj970513@naver.com>
2019-04-11 05:13:00 +0900

    헤더 불러오기

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
-    <header>{{ str }}</header>
+    <app-header></app-header>
+    {{ str }}
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue'
+
 export default {
   data: function() {
     return {
       str: 'HI~!'
     }
+  },
+  components: {
+    'app-header': AppHeader
   }
 }
 // new Vue({
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- Vue 파일은 최소 2단어로 이루어지며, 첫글자 대문자로, 카멜케이스 -->
+    <header>
+       헤더 가져오기~
+    </header>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
